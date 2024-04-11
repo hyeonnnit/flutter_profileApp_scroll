8f6760ff0286fc2aec50f1a6dc0f8d37549e445d
yeeun0118s <yeeun0118s@naver.com>
2024-04-11 17:29:41 +0900

    profile_tab / SingleTickerProviderStateMixin, _buildTabBar, _buildTabBarView

--- lib/components/profile_tab.dart
+++ lib/components/profile_tab.dart
@@ -1,25 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProfileTab extends StatefulWidget {
-
   @override
   _ProfileTabState createState() => _ProfileTabState();
 }
 
-class _ProfileTabState extends State<ProfileTab> {
+class _ProfileTabState extends State<ProfileTab>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         _buildTabBar(),
-        _buildTabBarView(),
+        Expanded(child: _buildTabBarView()),
       ],
     );
   }
-  Widget _buildTabBar(){
-    return SizedBox();
+
+  Widget _buildTabBar() {
+    return TabBar(
+      controller: _tabController,
+      tabs: [
+        Tab(icon: Icon(Icons.directions_car)),
+        Tab(icon: Icon(Icons.directions_transit)),
+      ],
+    );
   }
-  Widget _buildTabBarView(){
-    return SizedBox();
+
+  Widget _buildTabBarView() {
+    return TabBarView(
+      controller: _tabController,
+      children: [
+        Container(color: Colors.green),
+        Container(color: Colors.red),
+      ],
+    );
   }
 }
