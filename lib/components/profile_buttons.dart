daaca4444a38e12c3b075a6c3983d1408a919836
yeeun0118s <yeeun0118s@naver.com>
2024-04-11 16:47:23 +0900

    profile_buttons

--- lib/components/profile_buttons.dart
+++ lib/components/profile_buttons.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+class ProfileButtons extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _buildFollowButton(),
+        _buildMessageButton(),
+      ],
+    );
+  }
+  Widget _buildFollowButton(){
+    return SizedBox();
+  }
+  Widget _buildMessageButton(){
+    return SizedBox();
+  }
+}
